11e45268290ef931efc7a141dc53addfb85d9029
sylph <ccg0611a@gmail.com>
2024-06-05 16:14:09 +0900

    first commit

---
+++ src/main/java/com/sylph/sylph/SylphApplication.java
@@ -0,0 +1,13 @@
+package com.sylph.sylph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SylphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SylphApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sylph/sylph/SylphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sylph.sylph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SylphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
